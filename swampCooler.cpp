2e0100b678bca9d441bf10a7c30bc383b4f856e6
Tiff <thodges@nevada.unr.edu>
2023-05-08 21:01:13 -0700

    initial setup for functions

---
+++ swampCooler.cpp
@@ -0,0 +1,33 @@
+//This is the code for the arduino swamp cooler design
+//Group Name: thodges
+//Tiff Hodges
+
+
+//include files for libraries
+#include <Stepper.h>
+#include <LiquidCrystal.h>
+#include <DHT.h>
+
+//definitions
+
+//global variables
+
+void setup(){}
+void loop(){}
+void monitorWater(){}
+void currentState(){}
+void idle(){}
+void error(){}
+void turnOff(){}
+void timeAndDate(){}
+void controlVent(){}
+void airAndHumidity(){}
+void monitorWater(){}
+void fanMotor(){}
+void clock(){}
+void lightsOn(){}
+void lightsOff(){}
+void PIN_MODE(){}
+void WRITE_PIN(){}
+void adc_init(){}
+
