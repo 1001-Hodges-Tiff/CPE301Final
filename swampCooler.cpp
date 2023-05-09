dd7a44fb53fa5d1ed8b8808d14b0f72e66897cb1
hodges_tiffani <thodges@nevada.unr.edu>
2023-05-08 21:13:48 -0700

    adding registers to beginnning of document

--- swampCooler.cpp
+++ swampCooler.cpp
@@ -8,6 +8,22 @@
 #include <LiquidCrystal.h>
 #include <DHT.h>
 
+//registers
+
+//Timer 
+volatile unsigned char *myTCCR1A  = (unsigned char *) 0x80;
+volatile unsigned char *myTCCR1B  = (unsigned char *) 0x81;
+volatile unsigned char *myTCCR1C  = (unsigned char *) 0x82;
+volatile unsigned char *myTIMSK1  = (unsigned char *) 0x6F;
+volatile unsigned char *myTIFR1   = (unsigned char *) 0x36;
+volatile unsigned int  *myTCNT1   = (unsigned  int *) 0x84;
+
+//ADC
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+
 //definitions
 
 //global variables
